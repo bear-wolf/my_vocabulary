c24cf0756dae0f8d5ce632c6b02ffe59a296c4a6
andrew <andrii.baranov@peakscircle.software>
2024-04-26 13:48:52 +0300

    server: migration fix

--- server/migration/migrations/20240418131339-init-dictionary.js
+++ server/migration/migrations/20240418131339-init-dictionary.js
@@ -130,8 +130,8 @@ topicList.map(item => {
     item.created_at = Date.now().toString()
 })
 
-let words = [{
-        topic_uuid: topicList[0].uuid,
+let words = [
+    {
         picture: 'svgs/words/bear.svg',
         original: 'Bear',
         translate: JSON.stringify({
@@ -195,6 +195,7 @@ let words = [{
         voice: JSON.stringify({})
     }
 ]
+
 const wordList = [
     ...(words.map(item => {
         return {
@@ -254,6 +255,7 @@ const wordList = [
     })),
     ...(words.map(item => {
         return {
+            ...item,
             uuid: v4(),
             topic_uuid: topicList[7].uuid,
             created_at: Date.now().toString()
@@ -322,9 +324,524 @@ const wordList = [
             topic_uuid: topicList[15].uuid,
             created_at: Date.now().toString()
         }
+    })),
+
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[16].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[17].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[18].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[19].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[20].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[21].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[22].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[23].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[24].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[25].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[26].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[27].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[28].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[29].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[30].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[31].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[32].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[33].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[34].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[35].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[36].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[37].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[38].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[38].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[39].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[40].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[41].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[42].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[43].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[44].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[45].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[46].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[47].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[48].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[49].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[50].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[51].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[52].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[53].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[54].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[55].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[56].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[57].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[58].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[59].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[60].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[61].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[62].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[63].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[64].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[65].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[66].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[67].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[68].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[69].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[70].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[71].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[72].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[73].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[74].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[75].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[76].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[77].uuid,
+            created_at: Date.now().toString()
+        }
+    })),
+    ...(words.map(item => {
+        return {
+            ...item,
+            uuid: v4(),
+            topic_uuid: topicList[78].uuid,
+            created_at: Date.now().toString()
+        }
     }))
 ]
-
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
